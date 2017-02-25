c1c700de3f287335d456af300b7eea26cb047d3f
wosunzhiping <wosunzhiping@126.com>
2017-02-26 03:09:09 +0800

    增加suit部分，测试用例部分。但是在2.7或3.5下执行都出错

--- main.py
+++ main.py
@@ -3,17 +3,27 @@
 import time
 import logging
 import unittest
+import sys
+# sys.path.append('.\\lib\\Logger.py')
+# sys.path.append('.\\lib\\HTMLTestRunner.py')
 from lib.Logger import Logger
 from lib.HTMLTestRunner import HTMLTestRunner
+from testcase.admin_login_logout.admin_login_correction import AdminLoginCorrection
+from testcase.admin_login_logout.admin_login_failure import AdminLoginFail
 
-
+def suite():
+    test_suite = unittest.TestSuite()
+    loader = unittest.TestLoader()
+    test_suite.addTests(loader.loadTestsFromTestCase(AdminLoginCorrection))
+    test_suite.addTests(loader.loadTestsFromTestCase(AdminLoginFail))
+    return test_suite
 
 if __name__ == '__main__':
     logger = Logger().getlog()
     logger.info('测试开始...')
 
     #生成HTML格式的测试报告
-    report_path = './result/test_result_%s.html' % time.strftime(("%Y -%M-%D %H-%M-%S"))
+    report_path = './result/test_result_%s.html' % time.strftime("%Y-%m-%d %H-%M-%S")
     fp = open(report_path,'wb')
     runner = HTMLTestRunner(stream=fp,
                             title=u'XXX测试报告',
