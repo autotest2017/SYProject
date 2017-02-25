86f906cb4de2e366135a651895d59be08fa648b6
wosunzhiping <wosunzhiping@126.com>
2017-02-26 02:07:30 +0800

    增加生成HTML测试报告框架的内容

--- main.py
+++ main.py
@@ -4,10 +4,27 @@ import time
 import logging
 import unittest
 from lib.Logger import Logger
+from lib.HTMLTestRunner import HTMLTestRunner
+
 
 
 if __name__ == '__main__':
     logger = Logger().getlog()
     logger.info('测试开始...')
+
+    #生成HTML格式的测试报告
+    report_path = './result/test_result_%s.html' % time.strftime(("%Y -%M-%D %H-%M-%S"))
+    fp = open(report_path,'wb')
+    runner = HTMLTestRunner(stream=fp,
+                            title=u'XXX测试报告',
+                            description=u'测试用例执行情况：')
+    runner.run(suite())
+    fp.close()
+
+
+
+
+
+
     logger.info('测试结束。')
 
